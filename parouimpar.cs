45c80be56fc636653a016de3bc68937dbfd87af7
esterboldt <114186938+esterboldt@users.noreply.github.com>
2024-07-24 10:19:49 -0300

    Add files via upload

---
+++ parouimpar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Verificador de Par ou Ímpar");
+        Console.Write("Digite um número inteiro: ");
+
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero % 2 == 0)
+        {
+            Console.WriteLine($"{numero} é um número PAR.");
+        }
+        else
+        {
+            Console.WriteLine($"{numero} é um número ÍMPAR.");
+        }
+
+        Console.ReadLine();
+    }
+}
